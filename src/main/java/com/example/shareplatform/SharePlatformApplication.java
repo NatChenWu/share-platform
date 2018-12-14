d23625400271760a4d120c4e26123c54ee5f1cc6
alonso.chen <alonso.chen@pkfare.com>
2018-12-14 18:20:38 +0800

    first commit

---
+++ src/main/java/com/example/shareplatform/SharePlatformApplication.java
@@ -0,0 +1,14 @@
+package com.example.shareplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharePlatformApplication.class, args);
+    }
+
+}
+
