74881aea34fc03a835fc307d18a5992767eca304
weiyibin <371592215@qq.com>
2019-01-15 00:05:26 +0800

    mysql 调整为新版的驱动  去掉启动了的mapper扫描  config有一个就行

--- src/main/java/com/example/shareplatform/SharePlatformApplication.java
+++ src/main/java/com/example/shareplatform/SharePlatformApplication.java
@@ -1,11 +1,9 @@
 package com.example.shareplatform;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.shareplatform.api.*.dao.mapper")
 public class SharePlatformApplication {
 
     public static void main(String[] args) {
